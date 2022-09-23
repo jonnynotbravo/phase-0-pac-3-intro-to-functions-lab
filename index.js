0390cfe92af47fdb55de1f2da47a59462189afe9
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-09-22 22:38:06 -0400

    commit tree 5f5863115ea92cddd1d94d0b130f70c0e5ba5e95

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+    
+    return string.toUpperCase();
+
+}
+
+function whisper(string){
+
+    return string.toLowerCase();
+}
+
+function logShout(string){
+
+    console.log(string.toUpperCase());
+
+}
+
+function logWhisper(string){
+
+    console.log(string.toLowerCase());
+
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+    return string === string.toLowerCase() ? 'I can\'t hear you!' : string === 'Let\'s have dinner together!' ? 'I would love to!' : 'YES INDEED!';
+
+}
